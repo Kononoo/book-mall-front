c36226288600b43b755202f900449514d691bf31
ronan <321393295@qq.com>
2023-12-15 22:02:38 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -13,19 +13,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/',
+      name: 'Layout',
+      component: () => import('@/views/Layout/Layout.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login/Login.vue')
+    },
+    // 管理员后台页面
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/Admin/Layout/Layout.vue')
+    }
   ]
 })
 
