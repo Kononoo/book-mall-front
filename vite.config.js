72d29e72e44ce7646bfeffee6fbfc088cd5603a3
ronan <321393295@qq.com>
2023-12-15 19:25:23 +0800

    添加husky代码提交检查工具

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,28 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: './', // 设置打包路径
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // 设置 `@` 指向 `src` 目录
     }
+  },
+  server: {
+    // port: 4000, // 设置服务启动端口号
+    // open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true // 允许跨域
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
   }
 })
