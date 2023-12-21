19ec3959f82568d7c66b67d10f9c6d3912d7c085
wyatt_peng <w2386384506@gmail.com>
2023-12-21 13:06:13 +0800

    order2

--- src/views/body/LayoutOrder.vue
+++ src/views/body/LayoutOrder.vue
@@ -2,46 +2,47 @@
   <div class="cartContainer">
     <div class="content">
       <el-table
-          :data="cartList"
+          :data="cartList.records"
           :default-sort="{ prop: 'orderState', order: 'ascending' }"
           ref="table"
           empty-text="啥也没有呢"
       >
-        <el-table-column prop="orderID" label="订单号" width="100" sortable />
+        <el-table-column prop="id" label="订单号" width="100" sortable />
         <el-table-column label="图片">
           <template #default="scope">
-            <el-image :src="scope.row.coverImg" style="width: 80px"></el-image>
+            <el-image :src="scope.row.id" style="width: 80px"></el-image>
           </template>
         </el-table-column>
-        <el-table-column prop="title" label="书名"></el-table-column>
+        <el-table-column prop="id" label="书名"></el-table-column>
         <el-table-column label="价格" width="100">
           <template #default="scope">
-            <span>{{ scope.row.price.toFixed(2).toString() }}元</span>
+            <span>{{ scope.row.totalAmount.toFixed(2).toString() }}元</span>
           </template>
         </el-table-column>
-        <el-table-column prop="payState" label="付款状态" sortable>
+        <el-table-column prop="payStatus" label="付款状态" sortable>
           <template #default="scope">
-            <el-tag type="danger" v-if="scope.row.payState === 0">未付款</el-tag>
+            <el-tag type="danger" v-if="scope.row.payStatus === 0">未付款</el-tag>
             <el-tag v-else>已付款</el-tag>
           </template>
         </el-table-column>
         <el-table-column
-            prop="orderState"
+            prop="orderStatus"
             sortable
             :filters="stateFilter"
             :filter-method="filterState"
             label="交易状态"
         >
           <template #default="scope">
-            <el-tag v-if="scope.row.orderState === 0" type="danger">订单关闭</el-tag>
-            <el-tag v-else-if="scope.row.orderState === 2">已提交订单</el-tag>
-            <el-tag v-else-if="scope.row.orderState === 3" type="warning">已发货</el-tag>
-            <el-tag v-else-if="scope.row.orderState === 4" type="success">订单完成</el-tag>
+            <el-tag v-if="scope.row.orderStatus === 0" type="danger">订单关闭</el-tag>
+            <el-tag v-else-if="scope.row.orderStatus === 1">等待付款</el-tag>
+            <el-tag v-else-if="scope.row.orderStatus === 2">已提交订单</el-tag>
+            <el-tag v-else-if="scope.row.orderStatus === 3" type="warning">已发货</el-tag>
+            <el-tag v-else-if="scope.row.orderStatus === 4" type="success">订单完成</el-tag>
             <!-- <span>{{ getOrder(scope.row.orderState) }}</span> -->
           </template>
         </el-table-column>
         <el-table-column
-            prop="name"
+            prop="username"
             label="收货人姓名"
             sortable
             :filters="nameFilter"
@@ -67,54 +68,54 @@
         </el-table-column>
       </el-table>
     </div>
-    <el-dialog v-model="isDetail" :title="detail.title">
-      <div class="detailContainer">
-        <div class="content">
+    <el-dialog v-model="isDetail" :title="cartList.pages">
+      <div class="detailContainer" v-for="record in cartList.records">
+        <div class="content" v-if="record.id === 1737154732730851300">
           <div>
-            订单编号：<span>{{ detail.orderID }}</span>
+            订单编号：<span>{{ record.id }}</span>
           </div>
           <div>
-            收货人姓名：<span>{{ detail.name }}</span>
+            收货人姓名：<span>{{ record.username }}</span>
           </div>
           <div>
-            收货地址：<span>{{ detail.orderAddress }}</span>
+            收货地址：<span>{{ record.address }}</span>
           </div>
           <div>
-            订单价格：<span>{{ detail.price }}</span>
+            订单价格：<span>{{ record.totalAmount }}</span>
           </div>
           <div>
-            手机号码：<span>{{ detail.telephone }}</span>
+            手机号码：<span>{{ record.phone }}</span>
           </div>
           <div style="margin-top: 2rem">
             订单状态：
             <el-steps
-                :active="detail.orderState - (detail.payState === 1 ? 0 : 1)"
+                :active="record.orderStatus - (record.payStatus === 1 ? 0 : 1)"
                 simple
                 finish-status="success"
                 style="margin-top: 1rem"
             >
               <el-step
-                  :title="detail.orderState === 0 ? '订单关闭' : '提交订单'"
-                  :status="detail.orderState === 0 ? 'error' : 'success'"
+                  :title="record.orderStatus === 0 ? '订单关闭' : '提交订单'"
+                  :status="record.orderStatus === 0 ? 'error' : 'success'"
               ></el-step>
               <el-step
-                  :title="detail.payState === 1 ? '已付款' : '未付款'"
-                  :status="detail.payState === 1 ? 'success' : ''"
+                  :title="record.payStatus === 1 ? '已付款' : '未付款'"
+                  :status="record.payStatus === 1 ? 'success' : ''"
               ></el-step>
               <el-step
-                  :title="detail.orderState === 3 ? '已发货' : '等待发货'"
-                  :status="detail.orderState === 3 ? 'success' : ''"
+                  :title="record.orderStatus === 3 ? '已发货' : '等待发货'"
+                  :status="record.orderStatus === 3 ? 'success' : ''"
               ></el-step>
               <el-step
-                  :title="detail.orderState === 4 ? '订单完成' : '等待收货'"
-                  :status="detail.orderState === 4 ? 'success' : ''"
+                  :title="record.orderStatus === 4 ? '订单完成' : '等待收货'"
+                  :status="record.orderStatus === 4 ? 'success' : ''"
               ></el-step>
             </el-steps>
           </div>
         </div>
         <div class="footer">
           <el-button
-              v-if="detail.payState === 0 && detail.orderState === 2"
+              v-if="record.payStatus === 0 && record.orderStatus === 2"
               @click="payHandler"
               type="primary"
           >付款</el-button
@@ -137,7 +138,6 @@ export default {
     return {
       cartList: [],
       isDetail: false,
-      detail: [],
       stateFilter: [
         { text: "订单关闭", value: "0" },
         { text: "已提交订单", value: "2" },
@@ -208,7 +208,6 @@ export default {
               method: "post",
               url: `/order/update`,
               headers: {
-                token: store.state.token,
               },
               data: {
                 ...row,
@@ -280,27 +279,49 @@ export default {
 
 refresh() {
   // 模拟从后端获取的订单数据
-  const mockData = [
-    {
-      orderID: 1,
-      coverImg: new URL("@/assets/image/attack1.png",import.meta.url).href,
-      title: "Book 1",
-      price: 20.00,
-      payState: 0,
-      orderState: 2,
-      name: "John Doe",
-    },
-    {
-      orderID: 2,
-      coverImg: new URL("@/assets/image/attack2.png",import.meta.url).href,
-      title: "Book 2",
-      price: 25.00,
-      payState: 1,
-      orderState: 4,
-      name: "Jane Doe",
-    },
-    // 添加更多订单数据...
-  ];
+  const mockData = {
+    records: [
+      {
+        id: 1737154732730851300,
+        userId: 1,
+        username: "Morita Yota",
+        "totalAmount": 356,
+        "payAmount": 356,
+        payStatus: 1,
+        orderStatus: 1,
+        phone: "13578512654",
+        address: "上海",
+        detail: new URL("@/assets/image/三体.png",import.meta.url).href,
+        createTime: "2023-12-20 00:55:38",
+        checkoutTime: "2023-12-20 00:55:38",
+        idDeleted: 0
+      },
+      {
+        id: 1737150860029304800,
+        userId: 1,
+        username: "Morita Yota",
+        "totalAmount": 712,
+        "payAmount": 712,
+        payStatus: 0,
+        orderStatus: 1,
+        phone: "13578512654",
+        address: "wuhan",
+        detail: new URL("@/assets/image/三体.png",import.meta.url).href,
+        createTime: "2023-12-20 00:40:14",
+        checkoutTime: "2023-12-20 00:40:14",
+        idDeleted: 0
+      }
+    ],
+    "total": 2,
+    "size": 10,
+    "current": 1,
+    "orders": [],
+    "optimizeCountSql": true,
+    "searchCount": true,
+    "maxLimit": null,
+    "countId": null,
+    "pages": 1
+  };
 
   this.cartList = mockData;
 },
@@ -314,58 +335,33 @@ refresh() {
     //       token: store.state.token,
     //     },
     //   }).then((res) => {
-    //     // console.log(res.data);
-    //     let arr = [];
-    //     res.data.forEach((item) => {
-    //       Object.assign(item, ...item.book, item);
-    //       delete item["book"];
-    //       // console.log(item);
-    //       arr.push({ text: item.name, value: item.name });
-    //     });
-    //     // 去重
-    //     let newObj = {};
-    //     arr = arr.reduce((preVal, curVal) => {
-    //       newObj[curVal.text] ? "" : (newObj[curVal.text] = preVal.push(curVal));
-    //       return preVal;
-    //     }, []);
-    //     this.nameFilter = arr;
-    //     this.cartList = res.data.filter((item) => item.orderState !== 1);
+    //     console.log(res.data);
+    //     this.cartList = res.data;
     //   });
     // },
 
-    async changeDetail() {
-      // 模拟从后端获取订单详细信息
-      const mockDetail = {
-        orderID: 1,
-        name: "John Doe",
-        orderAddress: "123 Main St, City",
-        price: 20.00,
-        telephone: "123-456-7890",
-        orderState: 3,
-        payState: 3,
-        title: "订单一"
-      };
-
-      this.detail = mockDetail;
-      this.isDetail = true;
-    },
-  },
-    // 打开详细面板
-  //   async changeDetail(event) {
-  //     this.isDetail = true;
-  //     await service({
-  //       method: "get",
-  //       url: `/order/list/${store.state.userInfo.userID}?orderID=${event}`,
-  //       headers: {
-  //         token: store.state.token,
-  //       },
-  //     }).then((res) => {
-  //       this.detail = res.data.list[0];
-  //       // console.log(this.detail);
-  //     });
+  //   async changeDetail() {
+  //     // 模拟从后端获取订单详细信息
+  //     const mockDetail = {
+  //       orderID: 1,
+  //       name: "John Doe",
+  //       orderAddress: "123 Main St, City",
+  //       price: 20.00,
+  //       telephone: "123-456-7890",
+  //       orderState: 3,
+  //       payState: 3,
+  //       title: "订单一"
+  //     };
+  //
+  //     this.detail = mockDetail;
   //     this.isDetail = true;
   //   },
   // },
+    // 打开详细面板
+    async changeDetail() {
+      this.isDetail = true;
+    },
+  },
 
   async beforeMount() {
     // orderState=2时为待付款状态
